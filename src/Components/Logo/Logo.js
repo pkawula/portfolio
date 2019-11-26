aa6233047b5bb5c3cbefc849b7077e42474031bd
Piotr Kawula <kontakt@piotrkawula.pl>
2019-11-26 21:36:27 +0100

    Created skeleton for menu section

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from '../../assets/images/logo_3_gray.png';
-import styles from './Logo.module.scss';
+import React from "react";
+import logo from "../../assets/images/logo_3_gray.png";
+import styles from "./Logo.module.scss";
+import { NavLink } from "react-router-dom";
 
 const Logo = () => (
-    <a className={styles.container} href="/">
-        <img className={styles.container__img} src={logo} alt="Logo" />
-        <span className={styles.container__text}>piotrkawula.pl</span>
-    </a>
+  <NavLink className={styles.container} exact to="/">
+    <img className={styles.container__img} src={logo} alt="Logo" />
+    <span className={styles.container__text}>piotrkawula.pl</span>
+  </NavLink>
 );
 
 export default Logo;
