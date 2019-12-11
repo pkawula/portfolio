236f9a49e86fceb1038526e7aa102e6cc983406d
Piotr Kawula <kontakt@piotrkawula.pl>
2019-12-11 20:43:59 +0100

    Added ReCAPTCHA

--- src/Components/SubmitForm/SubmitForm.js
+++ src/Components/SubmitForm/SubmitForm.js
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "./SubmitForm.module.scss";
 import Button from "../Button/Button";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const SubmitForm = () => {
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
   return (
     <div className={styles.wrapper}>
       <p className={styles.wrapperTitle}>
         Show me that humanity is still in you {":)"}
       </p>
-      <span className={styles.wrapperCaptcha}>Captcha here</span>
+      <span className={styles.wrapperCaptcha}>
+        <ReCAPTCHA
+          sitekey="6LcrSccUAAAAAKYXV3UJy2N9iC6ATdH-OW5Lzjb-"
+          onChange={onChange}
+        />
+      </span>
       <Button>Send</Button>
     </div>
   );
