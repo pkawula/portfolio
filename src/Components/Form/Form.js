5312315724f51769efd206061b5b15262a4d8479
Piotr Kawula <kontakt@piotrkawula.pl>
2019-12-11 20:48:12 +0100

    Created Form Component

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <>
+        <form>
+          <label for="name"></label>
+          <label for="email"></label>
+          <label for="message"></label>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Form;
