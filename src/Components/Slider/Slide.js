baab2208699a7d30854497792e1eab077d7fb2ef
Piotr Kawula <kontakt@piotrkawula.pl>
2019-12-29 21:39:25 +0100

    Changes in slider component

--- src/Components/Slider/Slide.js
+++ src/Components/Slider/Slide.js
@@ -3,50 +3,69 @@ import styles from "./Slide.module.scss";
 import GithubImage from "../../assets/images/icons/github-dark.svg";
 import WebImage from "../../assets/images/icons/web.svg";
 
-const Slide = ({ title, description }) => {
+const Slide = ({ title, description, demo, code }) => {
   return (
     <section className={styles.slideWrapper}>
-      <div className={styles.slideWrapperControls}>
-        <span className={styles.slideWrapperControlsBtn}>Prev</span>
-        <span className={styles.slideWrapperControlsBtn}>Next</span>
+      <div className={styles.slideWrapperContainer}>
+        <div className={styles.slideWrapperDescription}>
+          <h3 className={styles.slideWrapperDescriptionTitle}>{title}</h3>
+          <p className={styles.slideWrapperDescriptionParagraph}>
+            {description}
+          </p>
+        </div>
+        <div className={styles.slideWrapperLinks}>
+          <a
+            href={demo}
+            rel="noreferrer noopener"
+            target="_blank"
+            className={styles.slideWrapperLinksLink}
+          >
+            <img
+              className={styles.slideWrapperLinksLinkImg}
+              src={WebImage}
+              alt="Vist Page"
+            />
+            <span className={styles.slideWrapperLinksLinkText}>Visit</span>
+          </a>
+          <a
+            href={code}
+            rel="noreferrer noopener"
+            target="_blank"
+            className={styles.slideWrapperLinksLink}
+          >
+            <span className={styles.slideWrapperLinksLinkText}>Code</span>
+            <img
+              className={styles.slideWrapperLinksLinkImg}
+              src={GithubImage}
+              alt="View Code"
+            />
+          </a>
+        </div>
       </div>
-      <div className={styles.slideWrapperDescription}>
-        <h3 className={styles.slideWrapperDescriptionTitle}>{title}</h3>
-        <p className={styles.slideWrapperDescriptionParagraph}>{description}</p>
-      </div>
-      <div className={styles.slideWrapperLinks}>
-        <button className={styles.slideWrapperLinksLink}>
-          <img
-            className={styles.slideWrapperLinksLinkImg}
-            src={WebImage}
-            alt="Vist Page"
-          />
-          <span className={styles.slideWrapperLinksLinkText}>Visit</span>
-        </button>
-        <button className={styles.slideWrapperLinksLink}>
-          <img
-            className={styles.slideWrapperLinksLinkImg}
-            src={GithubImage}
-            alt="Vist Website"
-          />
-          <span className={styles.slideWrapperLinksLinkText}>Code</span>
-        </button>
-      </div>
-      <div className={styles.slideWrapperGallery}>
+      <section className={styles.slideWrapperGallery}>
+        <h3 className={styles.slideWrapperGalleryTitle}>Gallery</h3>
         <div className={styles.slideWrapperGalleryControls}>
-          <span className={styles.slideWrapperGalleryControlsBtn}>
-            {"<"} Prev
-          </span>
-          <span className={styles.slideWrapperGalleryControlsBtn}>
-            Next {">"}
-          </span>
+          <span className={styles.slideWrapperGalleryControlsBtn}>{"<"}</span>
+          <span className={styles.slideWrapperGalleryControlsBtn}>{">"}</span>
         </div>
         <section className={styles.slideWrapperGallerySlides}>
-          <img alt="img"></img>
-          <img alt="img"></img>
-          <img alt="img"></img>
+          <img
+            className={styles.slideWrapperGallerySlidesSlide}
+            src="https://via.placeholder.com/1920x1080"
+            alt="img"
+          ></img>
+          <img
+            className={styles.slideWrapperGallerySlidesSlide}
+            src="https://via.placeholder.com/1920x1080"
+            alt="img"
+          ></img>
+          <img
+            className={styles.slideWrapperGallerySlidesSlide}
+            src="https://via.placeholder.com/1920x1080"
+            alt="img"
+          ></img>
         </section>
-      </div>
+      </section>
     </section>
   );
 };
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -2,15 +2,35 @@ import React from "react";
 import Slide from "./Slide";
 import styles from "./Slider.module.scss";
 
-const Slider = () => {
-  return (
-    <section className={styles.wrapper}>
-      <Slide
-        title="Project title"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto deleniti dolore fugiat incidunt beatae cum saepe quibusdam ipsum sed illum."
-      ></Slide>
-    </section>
-  );
-};
+class Slider extends React.Component {
+  state = {
+    repos: []
+  };
+  render() {
+    fetch("https://api.github.com/users/pkawula/repos")
+      .then(res => res.json())
+      .then(res => {
+        for (const data of res) {
+          const { id, name, html_url, description } = data;
+          this.setState({ repos: data });
+        }
+      });
+
+    return (
+      <section className={styles.wrapper}>
+        <div className={styles.wrapperControls}>
+          <span className={styles.wrapperControlsBtn}>Prev</span>
+          <span className={styles.wrapperControlsBtn}>Next</span>
+        </div>
+        {/* <Slide
+        {...title}
+        {...descritpion}
+        {...code}
+        {...demo}
+      ></Slide> */}
+      </section>
+    );
+  }
+}
 
 export default Slider;
