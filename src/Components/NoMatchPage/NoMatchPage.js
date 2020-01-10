6aafb54b19518bc721e2990c94d89e589d3ad87a
Piotr Kawula <kontakt@piotrkawula.pl>
2020-01-10 21:53:10 +0100

    Styled 404 page

--- src/Components/NoMatchPage/NoMatchPage.js
+++ src/Components/NoMatchPage/NoMatchPage.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import styles from "./NoMatchPage.module.scss";
+import { Link } from "react-router-dom";
 
 const NoMatchPage = () => (
-    <h1>It seems that you're lost. We have nothing here. Go to <Link to="/">homepage</Link></h1>
+  <section className={styles.wrapper}>
+    <h1 className={styles.wrapperError}>404</h1>
+    <p className={styles.wrapperInformation}>
+      It seems like you're lost on this page...
+    </p>
+    <Link to="/" className={styles.wrapperLink}>
+      Let's go back to homepage!
+    </Link>
+  </section>
 );
 
 export default NoMatchPage;
