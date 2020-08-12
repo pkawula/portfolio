cee198f1940309e69b5201df0bf085db5e7bb028
Piotr Kawula <kontakt@piotrkawula.pl>
2020-08-12 22:24:31 +0200

    Cleanup: fixed max width of skills and projects section

--- src/Components/SkillSection/SkillSection.js
+++ src/Components/SkillSection/SkillSection.js
@@ -3,20 +3,17 @@ import styles from "./SkillSection.module.scss";
 import List from "../List/List";
 import skillsArray from "../../assets/skills.json";
 
-// const knowSomethingAbout = data.skills.knowSomethingAbout;
-// const wantToLearn = data.skills.wantToLearn;
-
 const SkillSection = () => {
 
-    const sureAbout = skillsArray.sureAbout;
-    const knowSomethingAbout = skillsArray.knowSomethingAbout;
-    const wantToLearn = skillsArray.wantToLearn;
+  const sureAbout = skillsArray.sureAbout;
+  const knowSomethingAbout = skillsArray.knowSomethingAbout;
+  const wantToLearn = skillsArray.wantToLearn;
 
   return (
     <section className={styles.wrapper}>
-        <List imgok data={sureAbout}>...am sure about</List>
-        <List imglike data={knowSomethingAbout}>...tried and know something about</List>
-        <List imgstop data={wantToLearn}>...want to learn</List>
+      <List imgok data={sureAbout}>...am sure about</List>
+      <List imglike data={knowSomethingAbout}>...tried and know something about</List>
+      <List imgstop data={wantToLearn}>...want to learn</List>
     </section>
   );
 };
